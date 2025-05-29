f6b85bc63cd7b9e4840a68beb9dc65850dfff181
barkwolwol <barkwolwol@gmail.com>
2025-05-29 18:47:16 +0900

    소켓 url 변경
    소켓 url 변경

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
-const socket = io("http://localhost:3002");
+const socket = io("https://tictactoe-server-htl6.onrender.com");
 
 const App = () => {
   const [board, setBoard] = useState(Array(9).fill(null));
